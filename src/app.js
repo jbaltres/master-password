ed9885c58c0f571b8c393b53b997b7053f486943
Juergen Baltres <juergen.baltres@gmail.com>
2019-09-30 12:26:04 +0200

    get set unset

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+/*
+commands:
+
+set {key} {value}
+unset {key}
+get {key}
+*/
+
 function showProcessDetails() {
   console.log(`Node version: ${process.version}`);
   console.log(`Platform: ${process.platform}${process.arch}`);
@@ -6,27 +14,74 @@ function showProcessDetails() {
 
 showProcessDetails();
 
-function add() {
-  const a = parseInt(process.argv[2]);
-  const b = parseInt(process.argv[3]);
-  const result = a + b;
-  return console.log(`Result: ${result}`);
+// const userArgv = process.argv.slice(2);
+// const action = userArgv[0];
+// const key = userArgv[1];
+// const value = userArgv[2];
+
+//alternative solution with slice and array destructoring
+
+const [action, key, value] = process.argv.slice(2);
+
+console.log(action, key, value);
+
+//  It is enough to call the correct function and log the required parameters.
+
+function set(key, value) {
+  console.log("set", key, value);
 }
 
-add();
+function unset(key) {
+  console.log("unset", key);
+}
 
-// Leons Lösung
-// const firstArgument = parseInt(process.argv[2]);
-// const secondArgument = parseInt(process.argv[3]);
+function get(key) {
+  console.log("get", key);
+}
 
-// function add(a, b) {
-//   return a + b;
-// }
-// function sub(a, b) {
-//   return a - b;
-// }
+// call the correct function based on action
 
-// const addResult = add(firstArgument, secondArgument);
-// console.log(`Add Result ${addResult}`);
-// const subResult = sub(firstArgument, secondArgument);
-// console.log(`Sub Result ${subResult}`);
+function perform() {
+  if (action === "set") {
+    set(key, value);
+  } else if (action === "unset") {
+    unset(key);
+  } else if (action === "get") {
+    get(key);
+  } else {
+    throw new Error("unknown action");
+  }
+}
+
+perform();
+
+/*
+// solution 1:
+switch (action) {
+  case "get":
+    get(key);
+    break;
+  case "set":
+    set(key, value);
+    break;
+  case "unset":
+    unset(key);
+    break;
+  default:
+    throw new Error("unknown action");
+}
+
+
+// solution 3:
+const commands = {
+  set,
+  get,
+  unset
+};
+
+const command = commands[action];
+if (!command) {
+  throw new Error("unknown action");
+}
+command(key, value);
+*/
