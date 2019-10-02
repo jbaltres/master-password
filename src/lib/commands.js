de621b9f8ca172272ff75fbc37a41c7a0da48ff7
Juergen Baltres <juergen.baltres@gmail.com>
2019-10-02 09:18:38 +0200

    add request.url

--- src/lib/commands.js
+++ src/lib/commands.js
@@ -42,3 +42,5 @@ exports.executeCommand = function executeCommand(password, action, key, value) {
   }
   return command(password, key, value);
 };
+
+exports.get = get;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,28 @@
-let http = require("http");
-const { readSecrets } = require("./lib/secrets");
-const secrets = readSecrets();
-console.log(secrets);
-http
-  .createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(JSON.stringify(secrets));
-    res.end();
-  })
-  .listen(8080);
+const http = require("http");
+const url = require("url");
+const { get } = require("./lib/commands");
+
+const server = http.createServer(function(request, response) {
+  if (request.url === "/favicon.ico") {
+    return response.end();
+  }
+  if (request.url === "/") {
+    return response.end("Welcome to my secrets manager");
+  }
+
+  try {
+    const path = request.url.slice(1);
+    const URLobject = url.parse(path);
+    console.log(path);
+    console.log(URLobject);
+    const secret = get("pula", URLobject.pathname);
+
+    response.write(secret);
+  } catch (error) {
+    response.write("Can not read secret");
+  }
+
+  response.end();
+});
+
+server.listen(8080);
