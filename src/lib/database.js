d98984b876db7a97495d8c09b55c26f4afc55b53
Juergen Baltres <juergen.baltres@gmail.com>
2019-10-04 10:48:05 +0200

    example with Promise

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,3 +1,5 @@
+// Callback
+
 const { MongoClient } = require("mongodb");
 const MongoUrl = "mongodb://localhost:27017/MasterPassword";
 
@@ -15,3 +17,33 @@ MongoClient.connect(
     database.close();
   }
 );
+
+// Promise
+// const { MongoClient } = require("mongodb");
+
+// async function initDatabase() {
+//   // Connection URL
+//   const url = "mongodb://localhost:27017/master-password";
+//   // Database Name
+//   const dbName = "master-password";
+
+//   const client = new MongoClient(url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   });
+
+//   try {
+//     // Use connect method to connect to the Server
+//     await client.connect();
+
+//     console.log("Database connected");
+//     const db = client.db(dbName);
+//     const secretsCollection = await db.createCollection("secrets");
+//     console.log("Collection secrets created");
+//   } catch (error) {
+//     console.error(error.stack);
+//   }
+//   client.close();
+// }
+
+// initDatabase();
